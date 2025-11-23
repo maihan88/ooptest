71181fd3eead5a866ebcb422871865e92b191033
maihan88 <hanvani0808@gmail.com>
2025-11-23 19:56:47 +0700

    Refactor story list initialization and load from file
    Refactor story list initialization and add stories from file.

--- Main.java
+++ Main.java
@@ -19,18 +19,14 @@ public class Main {
         menu.addItem("Add new story");
         menu.addItem("Exit");
         
-        StoryList storyList=new StoryList();
-        storyList.add(new Story(1, 101, "De Men Phieu Luu Ky", 9.5, "To Hoai", true,'V'));
-        storyList.add(new Story(2, 102, "One Piece", 9.8, "Eiichiro Oda", false, 'J'));
-        storyList.add(new Story(3, 205, "Mat Biec", 8.9, "Nguyen Nhat Anh", true, 'V'));
-        storyList.add(new Story(4, 310, "Harry Potter", 9.2, "J.K. Rowling", true, 'A'));
-        storyList.add(new Story(5, 404, "Conan", 8.5, "Gosho Aoyama", false, 'J'));
+        StoryList storyList = new StoryList();
 
         while (true) {
             int choice = menu.getChoice();
             Scanner sc = new Scanner(System.in);
             switch (choice) {
                 case 1 -> {
+                    storyList.addFromFile("Stories.txt");
                     storyList.output();
                 }
                 case 2 -> {
